ee0511b888ee802834877e7168cf42b4c822640b
Blank0211 <105364016+Blank0211@users.noreply.github.com>
2022-06-20 18:49:35 +0530

    v???

--- batch rename.py
+++ batch rename.py
@@ -2,14 +2,17 @@ import os
 
 folder1 = input('Files\' Location: ')
 os.chdir(folder1)
-filelist = os.listdir()
+file_list = os.listdir()
+file_list.sort()
 
 new_name = input('Rename to: ')
 num = int(input('Starting number: ')) 
 
 
-for f in filelist:
-    os.rename(f, f'{new_name.title()} - {num}.txt')
+for f in file_list:
+    
+    f_name, f_ext = os.path.splitext(f)
+    os.rename(f, f'{new_name.title()} - {num}{f_ext}')
     num +=1
-
+    
 print(os.listdir())
