9cf7acd8b2489f8feac24e40718cab4604b4e2a6
Blank0211 <105364016+Blank0211@users.noreply.github.com>
2022-06-25 12:18:40 +0530

    Add files via upload
    For this particular script, you have to move it to the directory where your files (that you want to rename) are located.

---
+++ batch rename 2.py
@@ -0,0 +1,21 @@
+import os
+from sys import argv 
+ 
+file_list = os.listdir()
+file_list.sort()
+script = os.path.basename(argv[0])
+
+new_name = input('Rename to: ')
+num = int(input('Starting number: ')) 
+
+
+for f in file_list:
+    if f == script:
+        continue 
+    
+    f_name, f_ext = os.path.splitext(f)
+    os.rename(f, f'{new_name.title()} - {num}{f_ext}')
+    num +=1
+    
+    
+print(os.listdir())
